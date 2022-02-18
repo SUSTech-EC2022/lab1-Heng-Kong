3858a24ff0428eafe43ab997bf964348f3c29948
Heng-Kong <48233192+Heng-Kong@users.noreply.github.com>
2022-02-18 15:54:04 +0800

    Add files via upload

--- simpleEA.m
+++ simpleEA.m
@@ -30,13 +30,75 @@ fitness_pop=[];% record the best fitness in current population
 
 % Initialise a population
 %% TODO
-
+pop = randi([0,31],4,1);  %生成4个个体，0-31之间
+bin_pop = dec2bin(pop); %10进制转换为2进制
 
 % Evaluate the initial population
-%% TODO
+%% TODOs
+fitness = objective(pop); %计算种群中每个个体的适应度
+fitness_pop = [fitness_pop, max(fitness)] %获取最优适应度
+if bestSoFarFit < max(fitness)
+    [bestSoFarFit,i] = max(fitness);
+    bestSoFarSolution = pop(i);
+end
+
+nbGen = nbGen + 1; %迭代次数
+nbEval = nbEval + 4;  %计算适应度次数
+
+fitness_gen = [fitness_gen, bestSoFarFit];
+solution_gen = horzcat(solution_gen, bestSoFarSolution);
 
 % Start the loop
-while (nbEval<T) 
+while (nbEval<T)
+    crossoverProb = fitness./sum(fitness); % roulette-wheel selection
+    offspringGenes = [];
+    for i = 1:2
+        parentIndexes = [];
+        for j = 1:2
+            r = rand();
+            for index = 1:4
+                if r>sum(crossoverProb(1:index-1)) && r<=sum(crossoverProb(1:index))
+                    break;
+                end
+            end
+            parentIndexes = [parentIndexes, index];
+        end
+        crossoverPoint = randi(4);
+        offspringGenes = [offspringGenes; [bin_pop(parentIndexes(1),1:crossoverPoint), bin_pop(parentIndexes(2),crossoverPoint+1:end)]];
+        offspringGenes = [offspringGenes; [bin_pop(parentIndexes(2),1:crossoverPoint), bin_pop(parentIndexes(1),crossoverPoint+1:end)]];
+    end
+    mutationProb = 1/5;
+    for i = 1:4
+        isMutation = rand(1,5)<mutationProb;
+        offspringGenes(i,isMutation) = dec2bin('1'-offspringGenes(i,isMutation))';
+    end
+    bin_pop = offspringGenes;
+    population = bin2dec(bin_pop);
+    fitness = objective(population);
+    [A,index] = sort(fitness,1,'descend');
+    fitness_pop=[fitness_pop,A(1)]
+    for i = 1:4
+        if fitness(i) > bestSoFarFit
+            bestSoFarFit = fitness(i);
+            bestSoFarSolution = population(i,:);
+        end
+    end
+    nbEval = nbEval + 4;
+    nbGen = nbGen + 1;
+    fitness_gen=horzcat(fitness_gen, bestSoFarFit);
+    solution_gen=horzcat(solution_gen, bestSoFarSolution);
+end
+bestSoFarFit
+bestSoFarSolution
+
+figure,plot(1:nbGen,fitness_gen,'b') 
+title('Fitness\_Gen')
+
+figure,plot(1:nbGen,solution_gen,'b') 
+title('Solution\_Gen')
+
+figure,plot(1:nbGen,fitness_pop,'b') 
+title('Fitness\_Pop')
 % Reproduction (selection, crossver)
 %% TODO
 
@@ -44,8 +106,7 @@ while (nbEval<T)
 %% TODO
 
 
-bestSoFarFit
-bestSoFarSolution
+
 
 
 
